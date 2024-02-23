79064093a58cd8b9772b96b85e98c0cc6e9df974
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-24 01:25:15 +0530

    Changes in the documentation

--- 134_🍁📍_DRAWING-BOARD_.js
+++ 134_🍁📍_DRAWING-BOARD_.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("canvas"),
 toolBtns = document.querySelectorAll(".tool"),
 fillColor = document.querySelector("#fill-color"),
+sizeSlider= document.querySelector("#size-slider"),
 ctx = canvas.getContext("2d");
 
 
@@ -89,6 +90,8 @@ toolBtns.forEach(btn => {
 })
 
 
+
+sizeSlider.addEventListener("change" , () => brushWidth = sizeSlider.value);
 
 canvas.addEventListener('mousedown' , startDraw); 
 canvas.addEventListener('mousemove' , drawing); 

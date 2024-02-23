089c691f10ae56eabeeb9e06a2272b00959b0ab5
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-23 20:49:47 +0530

    Changes in the documentation

--- 134_🍁📍_DRAWING-BOARD_.js
+++ 134_🍁📍_DRAWING-BOARD_.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector("canvas"),
 toolBtns = document.querySelectorAll(".tool"),
-fillColor = document.querySelectorAll("#fill-color"),
+fillColor = document.querySelector("#fill-color"),
 ctx = canvas.getContext("2d");
 
 
@@ -16,7 +16,11 @@ window.addEventListener("load", () => {
 })
 
 const drawRect = (e) => {
-	ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY );
+	if(!fillColor.checked){
+		return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY );
+	}
+
+    ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY );
 }
 
 

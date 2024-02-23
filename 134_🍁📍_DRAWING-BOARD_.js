0785f588629491ec96aecbb88daf5dbc6180b357
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-23 21:20:26 +0530

    Changes in the documentation

--- 134_🍁📍_DRAWING-BOARD_.js
+++ 134_🍁📍_DRAWING-BOARD_.js
@@ -25,8 +25,9 @@ const drawRect = (e) => {
 
 
 const drawCircle = (e) => {
+	ctx.beginPath();
 	let radius = Math.sqrt(Math.pow((prevMouseX-e.offsetX),2) + Math.pow((prevMouseY-e.offsetY),2));
-	ctx.arc(prevMouseX, prevMouseY, 50, 0, 2 * Math.PI);
+	ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
 	ctx.stroke();
 }
 

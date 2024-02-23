d5194739507e9da9e446c7faff8dff7873ee4194
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-23 13:30:56 +0530

    Changes in the documentation

---
+++ 134_🍁📍_DRAWING-BOARD_.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector("canvas"),
+ctx = canvas.getContext("2d");
+
+window.addEventListener('load', () => {
+	canvas.width = canvas.offsetWidth;
+	canvas.height =canvas.offsetHeight;
+})
+
+const drawing = (e) => {
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+}

8e5908884b0a89c3e0829c3a67fdeb95871f0f79
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-24 11:40:10 +0530

    Changes in the documentation

--- 134_🍁📍_DRAWING-BOARD_.js
+++ 134_🍁📍_DRAWING-BOARD_.js
@@ -3,7 +3,7 @@ toolBtns = document.querySelectorAll(".tool"),
 fillColor = document.querySelector("#fill-color"),
 sizeSlider= document.querySelector("#size-slider"),
 colorBtns = document.querySelectorAll(".colors  .option"),
-// colorPicker = document.querySelector("#color-picker"),
+colorPicker = document.querySelector("#color-picker"),
 ctx = canvas.getContext("2d");
 
 
@@ -107,10 +107,11 @@ colorBtns.forEach(btn => {
 	})
 })
 
-// colorPicker.addEventListener("change" , () => {
-// 	colorPicker.parentElement.style.background = colorPicker.value;
-// 	colorPicker.parentElement.click();
-// })
+
+colorPicker.addEventListener("change" , () => {
+	colorPicker.parentElement.style.background = colorPicker.value;
+	colorPicker.parentElement.click();
+})
 
 canvas.addEventListener('mousedown' , startDraw); 
 canvas.addEventListener('mousemove' , drawing); 

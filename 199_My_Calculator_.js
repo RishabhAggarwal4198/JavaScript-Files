0c029fbea735ab926784a98273dd7aca7e47552b
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-04-05 23:58:28 +0530

    Changes in the documentaion

--- 199_My_Calculator_.js
+++ 199_My_Calculator_.js
@@ -2,6 +2,6 @@ let input = document.querySelector('input')
 let one = document.querySelector("#btnDiv:nth-child (1)")
 
 
-.addEventListener('click', () => {
+  one.addEventListener('click', () => {
   input.value = 1
 })

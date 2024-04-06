0e0f93d819e355b0850a77460fd46b057e84645c
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-04-06 23:02:34 +0530

    Changes in the documentaion

--- 199_My_Calculator_.js
+++ 199_My_Calculator_.js
@@ -1,13 +1,37 @@
 let input = document.querySelector('input')
 let num_1 = document.querySelector("#btnDiv button:nth-child(1)")
-// let one = document.querySelector("#btnDiv button:nth-child(1)")
-// let one = document.querySelector("#btnDiv button:nth-child(1)")
-// let one = document.querySelector("#btnDiv button:nth-child(1)")
-// let one = document.querySelector("#btnDiv button:nth-child(1)")
-// let one = document.querySelector("#btnDiv button:nth-child(1)")
+let num_2 = document.querySelector("#btnDiv button:nth-child(2)")
+let num_3 = document.querySelector("#btnDiv button:nth-child(3)")
+let num_4 = document.querySelector("#btnDiv button:nth-child(4)")
+let num_5 = document.querySelector("#btnDiv button:nth-child(5)")
+let num_6 = document.querySelector("#btnDiv button:nth-child(6)")
+let num_7 = document.querySelector("#btnDiv button:nth-child(7)")
+let num_8 = document.querySelector("#btnDiv button:nth-child(8)")
+let num_9 = document.querySelector("#btnDiv button:nth-child(9)")
+let num_0 = document.querySelector("#btnDiv button:nth-child(0)")
 
 
   num_1.addEventListener('click', () => {
   input.value += 1;
+})
+
+  num_2.addEventListener('click', () => {
+  input.value += 2;
+})
+
+  num_3.addEventListener('click', () => {
+  input.value += 3;
+})
+
+  num_4.addEventListener('click', () => {
+  input.value += 4;
+})
+
+  num_5.addEventListener('click', () => {
+  input.value += 5;
+})
+
+  num_6.addEventListener('click', () => {
+  input.value += 6;
 })
 
